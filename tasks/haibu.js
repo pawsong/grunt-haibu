51c591e21c8fd861cb1d35c650a5d4d130dd3129
Song Seunggeun <gifarangw@gmail.com>
2013-08-02 02:34:53 +0900

    First release, with no test

--- tasks/haibu.js
+++ tasks/haibu.js
@@ -16,7 +16,8 @@ module.exports = function(grunt) {
         glob = require('glob');
 
     // Constants
-    var TARBALL_PATH = '.deploy/app.tar';
+    var DEPLOY_TMP_DIR = '.deploy',
+        TARBALL_PATH = DEPLOY_TMP_DIR + path.sep + 'app.tar';
 
     // Local libraries
     var compress = require('./lib/compress')(grunt);
@@ -25,56 +26,174 @@ module.exports = function(grunt) {
         var self = this;
 
         var options = self.options({
-            include: '.haibuinclude',
-            appRoot: '.'
+            mode: '',
+            startScript: 'start.js',
+            includeFile: '.haibuinclude',
+            path: '.'
         });
 
-        var outDir = options.appRoot + '/out/server';
-        var appName = [grunt.file.readJSON('package.json').name, self.target].join('_');
+        var packageJSON,
+            appName,
+            appFullName;
 
-        // make files
-        function makeOutDir(callback) {
-            wrench.rmdirSyncRecursive(outDir, true);
+        function checkOptionExists(option) {
+            if (!options[option]) {
+                grunt.fail.warn('In normal mode, options.initScript must be specified.');
+            }
+        }
 
-            mkdirp(outDir, function (err) {
-                if (err) { return callback(err); }
+        checkOptionExists('path');
 
-                return callback(null);
+        packageJSON = grunt.file.readJSON(options.path + path.sep + 'package.json');
+
+        /*
+         * App full name should be unique becaise haibu clean method will clean up all the apps
+         * if their name equals to each other even when they are running on different port.
+         */
+        appName = options.appName || (packageJSON && packageJSON.name);
+
+        if (!appName) {
+            grunt.fail.warn('Cannot get application name');
+        }
+
+        appFullName = [appName, self.target].join('_');
+
+        if (fs.existsSync(options.startScript)) {
+            grunt.fail.warn(options.startScript + 'in app root path is reserved file name for haibu deployment.');
+        }
+
+        // Check mode
+        if (grunt.util._.include(['normal', 'static'], options.mode) === false) {
+            grunt.fail.warn('Mode ' + String(options.mode).cyan + ' not supported.');
+        }
+
+        if (options.mode === 'normal') {
+            checkOptionExists('initScript');
+        }
+
+        if (options.mode === 'static') {
+            checkOptionExists('staticDir');
+        }
+
+        // make directories
+        function makeDirs (callback) {
+            wrench.rmdirSyncRecursive(DEPLOY_TMP_DIR, true);
+            mkdirp(DEPLOY_TMP_DIR, function(err) {
+                callback(err);
             });
         }
 
-        function generateInitScript(callback) {
-            var template =
-                fs.readFileSync(
-                    path.resolve(__dirname, 'template', 'start.js.template'),
+        function generatePackageJSON(callback) {
+            if(options.mode === 'normal') {
+
+                if (!packageJSON.scripts) {
+                    packageJSON.scripts = {};
+                }
+                packageJSON.scripts.start = options.startScript;
+
+            } else if(options.mode === 'static') {
+
+                // Override packageJSON var with template.
+                packageJSON = fs.readFileSync(
+                    path.resolve(__dirname, 'template', 'package.json.template'),
                     {encoding: 'utf8'}
                 );
 
-            template = template.replace(/{{port}}/g, self.data.port);
-            template = template.replace(/{{initScript}}/g, options.initScript);
+                // Maybe better way...
+                packageJSON = packageJSON.replace(/{{appName}}/g, appName);
+                packageJSON = packageJSON.replace(/{{startScript}}/g, options.startScript);
 
-            fs.writeFile(outDir + path.sep + 'start.js', template, function (err) {
-                if (err) { return callback(err); }
-                return callback(null);
+                packageJSON = JSON.parse(packageJSON);
+
+            } else {
+                return callback("Not valid mode");
+            }
+
+            fs.writeFile(DEPLOY_TMP_DIR + path.sep + 'package.json',
+                JSON.stringify(packageJSON, null, 2), function(err) {
+                    callback(err);
+                });
+        }
+
+        function generateInitScript(callback) {
+
+            var template;
+            
+            if (options.mode === 'normal') {
+
+                template = fs.readFileSync(
+                    path.resolve(__dirname, 'template', 'normal.js.template'),
+                    {encoding: 'utf8'}
+                );
+                template = template.replace(/{{initScript}}/g, options.initScript);
+
+            } else if (options.mode === 'static') {
+
+                template = fs.readFileSync(
+                    path.resolve(__dirname, 'template', 'static.js.template'),
+                    {encoding: 'utf8'}
+                );
+                template = template.replace(/{{staticDir}}/g, options.staticDir);
+
+            } else {
+                callback("Not valid mode");
+            }
+
+            template = template.replace(/{{port}}/g, options.port);
+            
+            fs.writeFile(DEPLOY_TMP_DIR + path.sep + options.startScript, template, function(err) {
+                callback(err);
             });
         }
 
         function compressSrc(callback) {
-            var includePath = options.appRoot + path.sep + options.include;
+            var pkgExists,
+                packageFile;
+
+            var i;
+
+            var includePath = options.path + path.sep + options.includeFile;
 
             var files = [];
 
             fs.readFileSync(includePath).toString().split('\n').forEach(function (line) {
-                var parsedFiles = glob.sync(line, { cwd: options.appRoot });
+                var parsedFiles = glob.sync(line, { cwd: options.path });
 
                 parsedFiles.forEach(function(entry) {
                     var file = {
-                        src: [ options.appRoot + path.sep + entry ],
-                        orig: { expand: true, cwd: options.appRoot, src: [ '**.*' ] },
+                        src: [ options.path + path.sep + entry ],
+                        orig: { expand: true, cwd: options.path, src: [ '**.*' ] },
                         dest: entry
                     };
                     files.push(file);
                 });
+            });
+
+            // Add package.json
+            pkgExists = false;
+
+            packageFile = {
+                src: [DEPLOY_TMP_DIR + path.sep + 'package.json'],
+                orig: { expand: true, cwd: options.path, src: [ '**.*' ] },
+                dest: 'package.json'
+            };
+
+            for (i = 0; i < files.length; i++) {
+                if(files[i].dest === 'package.json') {
+                    pkgExists = true;
+                    files[i] = packageFile;
+                }
+            }
+
+            if(!pkgExists) {
+                files.push(packageFile);
+            }
+
+            // Add start script
+            files.push({
+                src: [DEPLOY_TMP_DIR + path.sep + options.startScript],
+                orig: { expand: true, cwd: options.path, src: [ '**.*' ] },
+                dest: options.startScript
             });
 
             // Compress
@@ -90,14 +209,14 @@ module.exports = function(grunt) {
 
         function cleanCurrentApp(callback) {
             var targetURL =
-                options.haibu_host + ':' + options.haibu_port + '/drones/' +
-                    appName + '/clean';
+                options.haibuHost + ':' + options.haibuPort + '/drones/' +
+                    appFullName + '/clean';
 
             request.post({
                 url: targetURL,
                 form: {
-                    user: options.username,
-                    name: appName
+                    user: options.userName,
+                    name: appFullName
                 }
             }, function (err, res, body) {
                 if(err) { return callback(err); }
@@ -108,9 +227,9 @@ module.exports = function(grunt) {
 
         function deploy(callback) {
             var targetURL =
-                options.haibu_host + ':' + options.haibu_port + '/deploy/' +
-                    options.username + '/' +
-                    appName;
+                options.haibuHost + ':' + options.haibuPort + '/deploy/' +
+                    options.userName + '/' +
+                    appFullName;
 
             fs.createReadStream(TARBALL_PATH)
                 .pipe(request.post({
@@ -123,21 +242,21 @@ module.exports = function(grunt) {
         function verifyPort(body, callback) {
             grunt.log.ok("");
             grunt.log.ok("Test application successfully spawned!");
-            grunt.log.ok("- User name : " + options.username);
-            grunt.log.ok("- App name : " + appName);
+            grunt.log.ok("- User name : " + options.userName);
+            grunt.log.ok("- App name : " + appFullName);
 
             var result = JSON.parse(body); //app information
 
-            if(result.drone.port == self.data.port) {
+            if(result.drone.port == options.port) {
                 callback(null);
             } else {
                 grunt.log.error("But port is unexpected...");
                 grunt.log.error("Used port: " + result.drone.port);
-                grunt.log.error("Expected: " + self.data.port);
+                grunt.log.error("Expected: " + options.port);
                 grunt.log.error("Something going wrong... contact server admin.");
 
                 callback("Running port " +
-                    result.drone.port + " is different from target port " + self.data.port);
+                    result.drone.port + " is different from target port " + options.port);
             }
         }
 
@@ -145,13 +264,17 @@ module.exports = function(grunt) {
         var done = this.async();
 
         async.waterfall([
-            makeOutDir,
+            makeDirs,
+            generatePackageJSON,
             generateInitScript,
             compressSrc,
             cleanCurrentApp,
             deploy,
             verifyPort
         ], function(err) {
+            var patt,
+                domain;
+
             // TODO: Remove files
 
 
@@ -163,15 +286,16 @@ module.exports = function(grunt) {
                 return;
             }
 
-            var patt = new RegExp( '^(.*:)//([a-z\-.]+)(:[0-9]+)?(.*)$');
+            patt = new RegExp( '^(.*:)//([a-z\-.]+)(:[0-9]+)?(.*)$');
+            domain = patt.exec(options.haibuHost)[2];
 
             grunt.log.ok("");
-            grunt.log.ok("Port is " + self.data.port + " which is right, expected.");
+            grunt.log.ok("Port is " + options.port + " which is right, expected.");
             grunt.log.ok("");
             grunt.log.ok("Test app is now started at:");
-            grunt.log.ok("- http://" + patt.exec(options.haibu_host)[2] + ':' + self.data.port);
+            grunt.log.ok("- http://" + domain + ':' + options.port);
             grunt.log.ok("or")
-            grunt.log.ok("- http://" + self.target + "." + patt.exec(options.haibu_host)[2]);
+            grunt.log.ok("- http://" + self.target + "." + domain);
             grunt.log.ok("");
             grunt.log.ok("Finished!");
 
